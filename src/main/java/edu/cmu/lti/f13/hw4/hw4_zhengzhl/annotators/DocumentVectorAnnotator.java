89090cb3c63e82c5ac5e31eb174787b6d707ee9a
hunterhector <hunterhector@gmail.com>
2013-10-23 18:21:59 -0400

    First working thing

--- src/main/java/edu/cmu/lti/f13/hw4/hw4_zhengzhl/annotators/DocumentVectorAnnotator.java
+++ src/main/java/edu/cmu/lti/f13/hw4/hw4_zhengzhl/annotators/DocumentVectorAnnotator.java
@@ -1,41 +1,96 @@
 package edu.cmu.lti.f13.hw4.hw4_zhengzhl.annotators;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+
+import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
-import org.apache.uima.cas.FSIterator;
+import org.apache.uima.fit.util.FSCollectionFactory;
+import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
-import org.apache.uima.jcas.cas.IntegerArray;
-import org.apache.uima.jcas.cas.StringArray;
-import org.apache.uima.jcas.tcas.Annotation;
+import org.apache.uima.resource.ResourceInitializationException;
 
 import edu.cmu.lti.f13.hw4.hw4_zhengzhl.typesystems.Document;
+import edu.cmu.lti.f13.hw4.hw4_zhengzhl.typesystems.Token;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 
 public class DocumentVectorAnnotator extends JCasAnnotator_ImplBase {
+	private StanfordCoreNLP pipeline;
+
+	public void initialize(UimaContext aContext)
+			throws ResourceInitializationException {
+		super.initialize(aContext);
+
+		Properties props = new Properties();
+		props.put("annotators", "tokenize");
+
+		pipeline = new StanfordCoreNLP(props);
+	}
 
 	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
 
-		FSIterator<Annotation> iter = jcas.getAnnotationIndex().iterator();
-		if (iter.isValid()) {
-			iter.moveToNext();
-			Document doc = (Document) iter.get();
+		// FSIterator<Annotation> iter = jcas.getAnnotationIndex().iterator();
+		// if (iter.isValid()) {
+		// iter.moveToNext();
+		// Document doc = (Document) iter.get();
+		// createTermFreqVector(jcas, doc);
+		// }
+
+		for (Document doc : JCasUtil.select(jcas, Document.class)) {
 			createTermFreqVector(jcas, doc);
 		}
 
 	}
+
 	/**
 	 * 
 	 * @param jcas
 	 * @param doc
 	 */
 
-	private void createTermFreqVector(JCas jcas, Document doc) {
+	private void createTermFreqVector(JCas aJCas, Document doc) {
 
 		String docText = doc.getText();
-		
-		//TO DO: construct a vector of tokens and update the tokenList in CAS
-		
 
+		Annotation document = new Annotation(docText);
+		pipeline.annotate(document);
+
+		// int docBegin = doc.getBegin();
+		// TO DO: construct a vector of tokens and update the tokenList in CAS
+
+		List<Token> tokens = new ArrayList<Token>();
+		Map<String, Integer> tokenCounts = new HashMap<String, Integer>();
+		for (CoreLabel coreToken : document.get(TokensAnnotation.class)) {
+			// int beginIndex = coreToken
+			// .get(CharacterOffsetBeginAnnotation.class);
+			// int endIndex = coreToken.get(CharacterOffsetEndAnnotation.class);
+			// Token token = new Token(aJCas, docBegin + beginIndex, endIndex);
+
+			String word = coreToken.word();
+			if (tokenCounts.containsKey(word)) {
+				tokenCounts.put(word, tokenCounts.get(word) + 1);
+			} else {
+				tokenCounts.put(word, 1);
+			}
+		}
+
+		for (Entry<String, Integer> tokenCount : tokenCounts.entrySet()) {
+			Token token = new Token(aJCas);
+			token.setFrequency(tokenCount.getValue());
+			token.setText(tokenCount.getKey());
+			token.addToIndexes();
+			tokens.add(token);
+		}
+
+		doc.setTokenList(FSCollectionFactory.createFSList(aJCas, tokens));
 	}
-
 }
--- src/main/java/edu/cmu/lti/f13/hw4/hw4_zhengzhl/casconsumers/RetrievalEvaluator.java
+++ src/main/java/edu/cmu/lti/f13/hw4/hw4_zhengzhl/casconsumers/RetrievalEvaluator.java
@@ -2,12 +2,18 @@ package edu.cmu.lti.f13.hw4.hw4_zhengzhl.casconsumers;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
 import org.apache.uima.cas.FSIterator;
 import org.apache.uima.collection.CasConsumer_ImplBase;
+import org.apache.uima.fit.util.FSCollectionFactory;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.cas.FSList;
 import org.apache.uima.resource.ResourceInitializationException;
@@ -15,7 +21,8 @@ import org.apache.uima.resource.ResourceProcessException;
 import org.apache.uima.util.ProcessTrace;
 
 import edu.cmu.lti.f13.hw4.hw4_zhengzhl.typesystems.Document;
-
+import edu.cmu.lti.f13.hw4.hw4_zhengzhl.typesystems.Token;
+import edu.cmu.lti.f13.hw4.hw4_zhengzhl.utils.Pair;
 
 public class RetrievalEvaluator extends CasConsumer_ImplBase {
 
@@ -25,13 +32,19 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 	/** query and text relevant values **/
 	public ArrayList<Integer> relList;
 
-		
+	private Set<String> globalWords;
+
+	private List<Map<String, Integer>> documents;
+
 	public void initialize() throws ResourceInitializationException {
 
 		qIdList = new ArrayList<Integer>();
 
 		relList = new ArrayList<Integer>();
 
+		globalWords = new HashSet<String>();
+
+		documents = new ArrayList<Map<String, Integer>>();
 	}
 
 	/**
@@ -43,25 +56,34 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 
 		JCas jcas;
 		try {
-			jcas =aCas.getJCas();
+			jcas = aCas.getJCas();
 		} catch (CASException e) {
 			throw new ResourceProcessException(e);
 		}
 
 		FSIterator it = jcas.getAnnotationIndex(Document.type).iterator();
-	
+
 		if (it.hasNext()) {
 			Document doc = (Document) it.next();
 
-			//Make sure that your previous annotators have populated this in CAS
+			// Make sure that your previous annotators have populated this in
+			// CAS
 			FSList fsTokenList = doc.getTokenList();
-			//ArrayList<Token>tokenList=Utils.fromFSListToCollection(fsTokenList, Token.class);
+			// ArrayList<Token>tokenList=Utils.fromFSListToCollection(fsTokenList,
+			// Token.class);
 
 			qIdList.add(doc.getQueryID());
 			relList.add(doc.getRelevanceValue());
-			
-			//Do something useful here
 
+			// Do something useful here
+			Map<String, Integer> tokenWithFreq = new HashMap<String, Integer>();
+			for (Token token : FSCollectionFactory.create(fsTokenList,
+					Token.class)) {
+				globalWords.add(token.getText());
+				tokenWithFreq.put(token.getText(), token.getFrequency());
+			}
+
+			documents.add(tokenWithFreq);
 		}
 
 	}
@@ -76,16 +98,31 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 
 		super.collectionProcessComplete(arg0);
 
+		List<List<Pair<Integer, Double>>> allScorePairs = new ArrayList<List<Pair<Integer, Double>>>();
+
+		Map<String, Integer> currentQuery = null;
 		// TODO :: compute the cosine similarity measure
-		
-		
-		
+		for (int i = 0; i < qIdList.size(); i++) {
+			int qid = qIdList.get(i);
+			int rel = relList.get(i);
+			Map<String, Integer> document = documents.get(i);
+			if (rel == 99) {
+				currentQuery = document;
+				allScorePairs.add(new ArrayList<Pair<Integer, Double>>());
+			} else {
+				double cosine = computeCosineSimilarity(currentQuery, document);
+				allScorePairs.get(allScorePairs.size() - 1).add(
+						Pair.of(rel, cosine));
+			}
+		}
+
 		// TODO :: compute the rank of retrieved sentences
-		
-		
-		
+		for (List<Pair<Integer, Double>> scorePairs : allScorePairs) {
+			Collections.sort(scorePairs);
+		}
+
 		// TODO :: compute the metric:: mean reciprocal rank
-		double metric_mrr = compute_mrr();
+		double metric_mrr = compute_mrr(allScorePairs);
 		System.out.println(" (MRR) Mean Reciprocal Rank ::" + metric_mrr);
 	}
 
@@ -95,10 +132,29 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 	 */
 	private double computeCosineSimilarity(Map<String, Integer> queryVector,
 			Map<String, Integer> docVector) {
-		double cosine_similarity=0.0;
+		double cosine_similarity = 0.0;
 
 		// TODO :: compute cosine similarity between two sentences
-		
+		double length1 = 0.0;
+		double length2 = 0.0;
+		for (String word : globalWords) {
+			double freq1 = 0.0;
+			if (queryVector.containsKey(word)) {
+				freq1 = queryVector.get(word);
+			}
+			double freq2 = 0.0;
+			if (docVector.containsKey(word)) {
+				freq2 = docVector.get(word);
+			}
+
+			cosine_similarity += freq1 * freq2;
+
+			length1 += freq1 * freq1;
+			length2 += freq2 * freq2;
+		}
+
+		cosine_similarity = cosine_similarity == 0 ? 0.0 : cosine_similarity
+				/ (Math.sqrt(length1) * Math.sqrt(length2));
 
 		return cosine_similarity;
 	}
@@ -107,11 +163,23 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 	 * 
 	 * @return mrr
 	 */
-	private double compute_mrr() {
-		double metric_mrr=0.0;
+	private double compute_mrr(List<List<Pair<Integer, Double>>> allScorePairs) {
+		double metric_mrr = 0.0;
 
 		// TODO :: compute Mean Reciprocal Rank (MRR) of the text collection
-		
+		for (List<Pair<Integer, Double>> scorePairs : allScorePairs) {
+			for (int i = 0; i < scorePairs.size(); i++) {
+				Pair<Integer, Double> scorePair = scorePairs.get(i);
+				if (scorePair.first == 1) {
+
+					metric_mrr += 1.0 / (i + 1);
+					break;
+				}
+			}
+		}
+
+		metric_mrr /= allScorePairs.size();
+
 		return metric_mrr;
 	}
 
